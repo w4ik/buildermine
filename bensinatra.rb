96ff7a65de29b12a8f07eac1a3bef7fb9b48754c
Greg Miller <w4ik@arrl.net>
2013-08-20 18:11:14 -0400

    added sinatra-contrib gem

--- bensinatra.rb
+++ bensinatra.rb
@@ -1,7 +1,11 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'sinatra/json'
 require './config/environments' #database configuration
 require './models/model'
+require './models/pokemon'
+require 'json'
+require 'bundler/setup'
 
 get '/' do
   #"hey this is Ben's Sinatra site. Might use it as an API site"
@@ -25,3 +29,30 @@ get '/models' do
   @models = Model.all
   erb :models
 end
+
+post '/submitpokemon' do
+  @pokemon = Pokemon.new(params[:pokemon])
+  if @pokemon.save
+    redirect '/viewpokemons'
+  else
+    "Sorry there was an error!"
+  end
+end
+
+get '/pokemons' do
+ #'Does this work?' #Yes!	
+  @models = Pokemon.all
+  erb :pokemons
+end
+
+get '/viewpokemons' do
+ @pokemons = Pokemon.all	
+ erb :viewpokemons
+end
+
+post '/testjson' do
+  data = JSON.parse(request.body.read)
+  json data
+end
+
+
